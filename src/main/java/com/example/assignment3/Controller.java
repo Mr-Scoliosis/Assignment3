ed3322898a5057bca9f9784676d85bcf38753e52
Andrew Barrett <andrewbarrett148@gmail.com>
2021-12-05 21:03:55 +0000

    IDs set in hello-view.
    First iteration of email testing implemented.
    
    First iteration of verification messages implemented.
    
    Code will not run at current log, as password related fields are still unused.

--- src/main/java/com/example/assignment3/Controller.java
+++ src/main/java/com/example/assignment3/Controller.java
@@ -2,8 +2,41 @@ package com.example.assignment3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class Controller {
+
+    public boolean verMail = false;
+    public boolean verPass = false;
+
+    @FXML
+    private Label verStatus;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private void verifyAction() {
+        final String usermail = email.getText();
+        final String userpass = password.getText();
+
+        if(usermail.matches("(.*)@(.*)") && usermail.matches("(.@)") && usermail.matches(".com$")) {
+            verMail = true;
+        }
+
+        if (verMail && verPass) {
+            verStatus.setText("Verification Succeeded.");
+        } else if (verMail) {
+            verStatus.setText("Verification Failed: Password too weak.");
+        } else if (verPass) {
+            verStatus.setText("Verification Failed: Enter a valid email.");
+        } else {
+            verStatus.setText("Verification Failed: Enter a valid email and password.");
+        }
+    }
     /*@FXML
     private Label welcomeText;
 
