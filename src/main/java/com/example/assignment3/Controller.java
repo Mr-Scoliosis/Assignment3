d24de6fe38de8c96f12ff995b73b560ade1fb8d2
Andrew Barrett <andrewbarrett148@gmail.com>
2021-12-05 21:51:02 +0000

    3rd iteration of email matching.

--- src/main/java/com/example/assignment3/Controller.java
+++ src/main/java/com/example/assignment3/Controller.java
@@ -24,7 +24,7 @@ public class Controller {
         String userMail = email.getText();
         String userPass = password.getText();
 
-        if(userMail.contains("@") && userMail.matches(".com$")) {
+        if(userMail.matches("(.*)@(.*).com")) {
             verMail = true;
         }
 
