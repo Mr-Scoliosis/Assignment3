309256c36138312139c16be75c1802cbdb0c1da0
Andrew Barrett <andrewbarrett148@gmail.com>
2021-12-05 20:03:11 +0000

    xml updated with proper reg page. Tutorial found at https://docs.oracle.com/javafx/2/get_started/fxml_tutorial.htm#CIHHGHJJ

--- src/main/java/com/example/assignment3/HelloApplication.java
+++ src/main/java/com/example/assignment3/HelloApplication.java
@@ -2,7 +2,6 @@ package com.example.assignment3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -15,13 +14,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHGap(10);
-        grid.setVGap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        stage.setTitle("Login");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
