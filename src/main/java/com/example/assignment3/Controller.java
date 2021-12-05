1b1654cdd9590d80db4c9403608d2367fadb3a70
Andrew Barrett <andrewbarrett148@gmail.com>
2021-12-05 22:30:56 +0000

    2nd iteration of password checking, needs refinement.

--- src/main/java/com/example/assignment3/Controller.java
+++ src/main/java/com/example/assignment3/Controller.java
@@ -3,12 +3,13 @@ package com.example.assignment3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.*;
-import javafx.event.*;
+import java.util.Arrays;
 
 public class Controller {
 
     public boolean verMail = false;
     public boolean verPass = false;
+    public Integer passStrength = 0;
 
     @FXML
     private Label verStatus;
@@ -21,6 +22,13 @@ public class Controller {
 
     @FXML
     private void verifyAction() {
+        String[] digitList = {"1","2","3","4","5","6","7","8","9","0"};
+        String[] charList = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        String[] specList = {"!","@","?","%","&","*"};
+        int isDigit = 0;
+        int isChara = 0;
+        int isSpec = 0;
+        int isLong = 0;
         String userMail = email.getText();
         String userPass = password.getText();
 
@@ -28,7 +36,29 @@ public class Controller {
             verMail = true;
         }
 
-        if(userPass.contains("[abcdefghijklmnopqrstuvwxyz]") && userPass.contains("[0-9]")) {
+        for (int i = 0; i < digitList.length; i++) {
+            String var = digitList[i];
+            if(userPass.contains(var)) {
+                isDigit = 1;
+            }
+        for (int k = 0; k < charList.length; k++) {
+            String vark = charList[k];
+            if(userPass.contains(vark)) {
+                isChara = 1;
+            }
+        for (int j = 0; j < specList.length; j++) {
+            String varj = specList[j];
+            if(userPass.contains(varj)) {
+                isSpec = 1;
+            }
+        }
+        int length = userPass.length();
+        if( length > 7) {
+            isLong = 1;
+        }
+
+        passStrength = isChara + isLong + isDigit + isSpec;
+        if (passStrength == 4) {
             verPass = true;
         }
 
