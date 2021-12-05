546696bc3fb70a2941f77078fbde04e45de8cee7
Andrew Barrett <andrewbarrett148@gmail.com>
2021-12-05 18:48:01 +0000

    Creating the grid

--- src/main/java/com/example/assignment3/HelloApplication.java
+++ src/main/java/com/example/assignment3/HelloApplication.java
@@ -2,7 +2,10 @@ package com.example.assignment3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +15,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHGap(10);
+        grid.setVGap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
